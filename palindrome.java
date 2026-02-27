74dbf2c95168df8915cb8144d9fde5ab7ce6b6b0
Mohammed farhan <mohammedfarhanyusuf@gmail.com>
2026-02-27 07:03:13 +0000

    Added palindrome checker file

---
+++ palindrome.java
@@ -0,0 +1,17 @@
+class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("   Welcome to PalindromeChecker");
+        System.out.println("==================================");
+
+        System.out.println("Application Name : PalindromeChecker App");
+        System.out.println("Version          : 1.0");
+        System.out.println("Author           : Student Project");
+
+        System.out.println("----------------------------------");
+        System.out.println("Starting application...");
+        System.out.println("----------------------------------");
+    }
+}
