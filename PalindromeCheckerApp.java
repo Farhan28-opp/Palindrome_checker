270bda44fbeaf0eb6980b1bca4946273d201efff
Mohammed farhan <mohammedfarhanyusuf@gmail.com>
2026-03-06 08:33:46 +0530

    Create PalindromeCheckerApp.java

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "radar";
+
+        // Convert string to character array
+        char[] characters = word.toCharArray();
+
+        // Two pointers
+        int start = 0;
+        int end = characters.length - 1;
+
+        boolean isPalindrome = true;
+
+        // Compare characters using two-pointer technique
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+    }
+}
