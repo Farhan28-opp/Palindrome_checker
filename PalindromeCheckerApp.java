7d2645d2447da1e58ea758773a80080ceef3a6ca
Mohammed farhan <mohammedfarhanyusuf@gmail.com>
2026-03-06 08:28:24 +0530

    Implement palindrome checker application

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+       
+        if (word.equals(reversed)) {
+            System.out.println("The string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
+        }
+
+    }
+}
