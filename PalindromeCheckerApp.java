366499f1a5e16ba2831ac4c2ecdf0de72a2b5214
Mohammed farhan <mohammedfarhanyusuf@gmail.com>
2026-03-06 08:43:41 +0530

    Add PalindromeCheckerApp to check palindromes
    Implement palindrome checker using Stack and Queue

---
+++ PalindromeCheckerApp.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Create Stack and Queue
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push and Enqueue characters
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue and pop
+        for (int i = 0; i < word.length(); i++) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
