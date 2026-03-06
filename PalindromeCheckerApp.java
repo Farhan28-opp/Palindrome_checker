5bd0cf1720cf15015bca9d58d50a8247576a3f08
Mohammed farhan <mohammedfarhanyusuf@gmail.com>
2026-03-06 10:10:31 +0530

    Implement palindrome checking methods
    Added methods for palindrome checking using two-pointer, stack, and deque approaches.

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,36 +1,88 @@
 import java.util.Stack;
-import java.util.Queue;
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class PalindromeCheckerApp {
 
-    public static void main(String[] args) {
+    
+    public static boolean twoPointerCheck(String input) {
+        int start = 0;
+        int end = input.length() - 1;
 
-     
-        String word = "level";
+        while (start < end) {
+            if (input.charAt(start) != input.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 
+    
+    public static boolean stackCheck(String input) {
         Stack<Character> stack = new Stack<>();
-        Queue<Character> queue = new LinkedList<>();
 
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            stack.push(ch);      // LIFO
-            queue.add(ch);       // FIFO
+        for (int i = 0; i < input.length(); i++) {
+            stack.push(input.charAt(i));
         }
 
-        boolean isPalindrome = true;
-        while (!queue.isEmpty()) {
-            if (queue.remove() != stack.pop()) {
-                isPalindrome = false;
-                break;
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed += stack.pop();
+        }
+
+        return input.equals(reversed);
+    }
+
+    
+    public static boolean dequeCheck(String input) {
+        Deque<Character> deque = new LinkedList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            deque.addLast(input.charAt(i));
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
             }
         }
 
-     
-        if (isPalindrome) {
-            System.out.println("The string \"" + word + "\" is a Palindrome.");
-        } else {
-            System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
-        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String word = "racecar";
+
+
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointerCheck(word);
+        long end1 = System.nanoTime();
+
+      
+        long start2 = System.nanoTime();
+        boolean result2 = stackCheck(word);
+        long end2 = System.nanoTime();
+
+
+        long start3 = System.nanoTime();
+        boolean result3 = dequeCheck(word);
+        long end3 = System.nanoTime();
+
+      
+        System.out.println("Palindrome Check Results for: " + word);
+        System.out.println("-----------------------------------");
+
+        System.out.println("Two Pointer Result: " + result1 +
+                " | Time: " + (end1 - start1) + " ns");
+
+        System.out.println("Stack Method Result: " + result2 +
+                " | Time: " + (end2 - start2) + " ns");
+
+        System.out.println("Deque Method Result: " + result3 +
+                " | Time: " + (end3 - start3) + " ns");
     }
 }
